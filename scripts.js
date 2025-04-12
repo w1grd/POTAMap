fb5f3ca045740b7368acb940c96d4901e136ca80
Perry Donham <perry@w1grd.radio>
2025-04-12 12:52:39 -0400

    Snapshot with pulsing markers for new and active

--- scripts.js
+++ scripts.js
@@ -2499,7 +2499,7 @@ async function displayParksOnMap(map, parks, userActivatedReferences = null, lay
         //Set up data block
         //
         const tooltipText = currentActivation
-            ? `${reference}: ${name} <br> ${currentActivation.activator} on ${currentActivation.frequency} kHz (${currentActivation.mode})${currentActivation.comments ? ` — ${currentActivation.comments}` : ''}`
+            ? `${reference}: ${name} <br> ${currentActivation.activator} on ${currentActivation.frequency} kHz (${currentActivation.mode})${currentActivation.comments ? ` <br> ${currentActivation.comments}` : ''}`
             : `${reference}: ${name} (${parkActivationCount} activations)`;
 
         marker
