3d012ea3c8224fa7099726609f09dad85ace5295
Perry Donham <perry@w1grd.radio>
2025-08-26 09:52:43 -0400

    Change allparks period back to 6 days (was 8)

--- scripts.js
+++ scripts.js
@@ -281,7 +281,7 @@ function clearPqlFilterDisplay() {
 let activationToggleState = 0; // 0: Show all, 1: Show my activations, 2: Remove my activations
 let spots = []; //holds spot info
 const appVersion = "20250412a"; // manually update as needed
-const cacheDuration = (24 * 60 * 60 * 1000) * 2; // 8 days in milliseconds
+const cacheDuration = (24 * 60 * 60 * 1000) * 6; // 8 days in milliseconds
 
 // See if we are in desktop mode
 const urlParams = new URLSearchParams(window.location.search);
